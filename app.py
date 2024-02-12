95d6f5947b1471d74571cabb45a3eac978c56044
cniclas <niclas.carlstrom88@gmail.com>
2024-02-12 15:33:47 -0500

    remove obsolete code

--- app.py
+++ app.py
@@ -46,18 +46,5 @@ def filter_data():
 
     return render_template('index.html', selected_version='v75', interval_inputs=all_filters_html)
 
-
-@app.route('/add_filter')
-def add_filter():
-    global interval_inputs
-    interval_inputs.append(IntervalInputJackpot("New Filter", f"filter_{len(interval_inputs) + 1}"))  # Generate unique property name
-    return jsonify({'html': interval_inputs[-1].generate_html()})  # Return HTML for the new filter
-
-@app.route('/', methods=['POST'])
-def apply_filters():
-    global interval_inputs
-    # ... (Handle form submission and data filtering using interval_inputs)
-    return render_template('index.html', interval_inputs=interval_inputs)
-
 if __name__ == '__main__':
     app.run(debug=True)

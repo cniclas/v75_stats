f27ea8ad039930a3be13e494ac7e2e55f0c3520d
cniclas <niclas.carlstrom88@gmail.com>
2024-02-22 13:35:49 -0500

    persistent button text states

--- static/script.js
+++ static/script.js
@@ -1,43 +1,66 @@
-let allCheckBoxesState = true;
+
+let allBanorState = localStorage.getItem('allBanorState') !== null ?
+                    localStorage.getItem('allBanorState') === 'true' : 
+                    true; 
+
+let allMonthsState = localStorage.getItem('allMonthsState') !== null ?
+                     localStorage.getItem('allMonthsState') === 'true' : 
+                     true;
+
 function alla_banor_click() {
     const locationSelectionDiv = document.querySelector('.banor-checkboxes');
     const checkboxes = locationSelectionDiv.querySelectorAll('input[type="checkbox"]');
 
-    allCheckBoxesState = !allCheckBoxesState;
+    allBanorState = !allBanorState; 
 
-    // Loop through and uncheck each checkbox
     for (const checkbox of checkboxes) {
-        checkbox.checked = allCheckBoxesState;
+        checkbox.checked = allBanorState;
     }
 
     const toggleButton = document.getElementById('alla_banor_toggle_button');
-    if (allCheckBoxesState){
+    if (allBanorState) {
         toggleButton.textContent = "Avmarkera alla banor";
-    }
-    else{
+    } else {
         toggleButton.textContent = "Markera alla banor";
     }
-    
+
+    localStorage.setItem('allBanorState', allBanorState); 
 }
 
-let allMonthsState = true;
 function all_months_click() {
     const locationSelectionDiv = document.querySelector('.selected_months');
     const checkboxes = locationSelectionDiv.querySelectorAll('input[type="checkbox"]');
 
     allMonthsState = !allMonthsState;
 
-    // Loop through and uncheck each checkbox
     for (const checkbox of checkboxes) {
         checkbox.checked = allMonthsState;
     }
 
     const toggleButton = document.getElementById('all_months_toggle_button');
-    if (allMonthsState){
+    if (allMonthsState) {
         toggleButton.textContent = "Avmarkera alla månader";
-    }
-    else{
+    } else {
         toggleButton.textContent = "Markera alla månader";
     }
-    
+
+    localStorage.setItem('allMonthsState', allMonthsState);
+}
+
+window.onload = function() {  
+    // Banor Button State
+    const banorButton = document.getElementById('alla_banor_toggle_button');
+    if (allBanorState) {
+        banorButton.textContent = "Avmarkera alla banor";
+    } else {
+        banorButton.textContent = "Markera alla banor";
+    }
+
+    // Month Button State
+    const monthButton = document.getElementById('all_months_toggle_button');
+    if (allMonthsState) {
+        monthButton.textContent = "Avmarkera alla månader";
+    } else {
+        monthButton.textContent = "Markera alla månader";
+    }
 }
